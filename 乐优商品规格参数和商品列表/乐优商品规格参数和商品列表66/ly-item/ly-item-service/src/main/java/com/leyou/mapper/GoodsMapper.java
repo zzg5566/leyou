c2bd14d6398745d480b8b8694bd011efc2b52b9e
gzzitcast <gzzitcast5566@163.com>
2019-07-01 20:04:56 +0800

    删除目录乐优商品规格参数和商品列表/乐优商品规格参数和商品列表66

--- 乐优商品规格参数和商品列表/乐优商品规格参数和商品列表66/ly-item/ly-item-service/src/main/java/com/leyou/mapper/GoodsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.leyou.mapper;
-
-import com.leyou.common.mapper.BaseMapper;
-import com.leyou.entity.Spu;
-
-public interface GoodsMapper  extends BaseMapper<Spu>{
-}
--- 乐优商品规格参数和商品列表/乐优商品规格参数和商品列表66/ly-item/ly-item-service/src/main/java/com/leyou/mapper/SpecMapper.java
+++
@@ -1,7 +0,0 @@
-package com.leyou.mapper;
-
-import com.leyou.entity.Spec;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SpecMapper extends Mapper<Spec> {
-}
